124eef9ea00e763e1ad1d3d1aee2140b67e1eff5
Sıtkı Efe Kılınç <kilinc12efe@gmail.com>
2025-12-16 17:54:34 +0300

    projectStart

---
+++ EEZBankServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EEZBankServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
