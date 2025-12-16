ae590b64565ddfb8eff0f833705814bee6f972fe
Sıtkı Efe Kılınç <kilinc12efe@gmail.com>
2025-12-16 20:03:29 +0300

    addingCustomTagHelper

---
+++ EEZBankServer/Extensions/ServiceCardTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace EEZBank.TagHelpers 
+{
+    [HtmlTargetElement("service-icon")]
+    public class ServiceIconTagHelper : TagHelper
+    {
+        public string Symbol { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "service-icon");
+            output.Content.SetHtmlContent($"<i class='fas fa-{Symbol}'></i>");
+        }
+    }
+
+}
